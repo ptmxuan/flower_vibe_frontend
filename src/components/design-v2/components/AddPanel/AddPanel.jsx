7a23926954c0b7d265ad0c3ef040635688d553fc
VanTuoi <vantuoi.dev@gmail.com>
2024-11-22 10:46:47 +0700

    feat: search ingredient

--- src/components/design-v2/components/AddPanel/AddPanel.jsx
+++ src/components/design-v2/components/AddPanel/AddPanel.jsx
@@ -1,29 +1,53 @@
 import { useContext, useState } from 'react';
-import { Collapse } from 'antd';
+import { Collapse, Input } from 'antd';
 import './AddPanel.sass';
 import { AppContext } from '../../AppContext';
 import { AddIngredient } from './AddIngredient/AddIngredient';
 import { Typography } from 'antd';
-import { CaretDownOutlined } from '@ant-design/icons';
 
 const { Title } = Typography;
 const { Panel } = Collapse;
+const { Search } = Input;
 
 export const AddPanel = () => {
   const { addButtonList } = useContext(AppContext);
   const [activePanel, setActivePanel] = useState(['Hoa hồng']); // Active panel mặc định
+  const [searchTerm, setSearchTerm] = useState(''); // Lưu trữ từ khóa tìm kiếm
 
   const onCollapseChange = (keys) => {
     setActivePanel(keys); // Cập nhật trạng thái collapse đang mở
   };
 
+  const onSearch = (value) => {
+    setSearchTerm(value); // Cập nhật từ khóa tìm kiếm
+    // Tìm panel nào chứa thành phần có tên khớp với từ khóa
+    const matchedKey = Object.keys(addButtonList).find((key) =>
+      addButtonList[key].some((ingredient) =>
+        ingredient.toLowerCase().includes(value.toLowerCase())
+      )
+    );
+    if (matchedKey) {
+      setActivePanel([matchedKey]); // Mở panel chứa thành phần khớp
+    } else {
+      setActivePanel([]); // Đóng tất cả panel nếu không tìm thấy
+    }
+  };
+
   return (
     <div className="ingred_adder hidden-element">
       <Title level={4}>Danh sách các thành phần thiết kế</Title>
-      <Collapse 
-        accordion 
-        activeKey={activePanel} 
-        onChange={onCollapseChange} 
+      {/* Ô tìm kiếm */}
+      <Search
+        size='large'
+        placeholder="Nhập tên thành phần"
+        allowClear
+        onSearch={onSearch}
+        style={{ marginBottom: 16 }}
+      />
+      <Collapse
+        accordion
+        activeKey={activePanel}
+        onChange={onCollapseChange}
         className="ingred_adder__collapse"
       >
         {Object.keys(addButtonList).map((key) => (
@@ -31,7 +55,7 @@ export const AddPanel = () => {
             key={key}
             header={
               <div style={{ display: 'flex', alignItems: 'center' }}>
-                <span style={{fontSize: 14, fontWeight: 500}}>{key}</span>
+                <span style={{ fontSize: 14, fontWeight: 500 }}>{key}</span>
               </div>
             }
           >
