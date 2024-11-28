df647db719b9dcac66eae734c1f3c25c435262bc
VanTuoi <vantuoi.dev@gmail.com>
2024-11-28 13:05:58 +0700

    fix: pay

---
+++ src/pages/ManageChuDe.js
@@ -0,0 +1,9 @@
+import ProductsBody from "@/components/manage/products/ProductsBody";
+
+export default function ManageChuDePage() {
+  return (
+    <div className="ManageProductPage">
+      <ProductsBody />
+    </div>
+  );
+}
